3bb812eb83f125b53d72b43200ecd702a8de61f6
Bl4ck_Dr460n <49341995+Bl4ckDr460n@users.noreply.github.com>
2019-04-17 09:05:24 +0700

    Add files via upload

---
+++ ping.sh
@@ -0,0 +1,23 @@
+biru='\033[34;1m' #biru
+green='\e[0;32m' #hijau muda
+daun='\033[32;1m' #ijo
+purple='\033[35;1m' #purple
+cyyan='\033[36;1m' #cyan
+merah='\033[31;1m' #merah
+putih='\033[37;1m' #pur
+tai='\033[33;1m' #kuning
+clear
+echo $merah "[]======================[]"
+echo $daun  "    Author Bl4ckD460n   "
+echo $merah "[]======================[]"
+echo 
+echo	
+echo $biru "Masukan Target Ping Contoh"$putih" google.com"
+echo $tai
+read -p "dr460n >> " ping
+clear
+cowsay -f kiss.cow "Bl4ckDr460n"
+echo $daun
+ping $ping
+
+
